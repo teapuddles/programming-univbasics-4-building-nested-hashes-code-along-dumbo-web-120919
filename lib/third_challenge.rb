4e8cbcd8151ebc4ae8968d522af85931ab3bd85a
Kevin Gleeson <kingsley.gleeson@gmail.com>
2019-11-29 22:21:27 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,16 +3,16 @@ def third_challenge
    :montague => {
       :patriarch => {
         name: "Lord Montague", 
-        age: "53"}
+        age: "53"
       },
       :matriarch => {
         name: "Lady Montague", 
-        age: "54"}
+        age: "54"
       },  
       :hero => {
         name: "Romeo", 
         age: "15",
-        status: "alive"}
+        status: "alive"
       },  
       :hero_friends => []
    }, 
