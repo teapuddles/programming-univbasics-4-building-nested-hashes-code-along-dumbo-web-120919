b4a34a0d59339401a79afbf7342686fab39e7d45
Kevin Gleeson <kingsley.gleeson@gmail.com>
2019-11-29 22:40:38 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,7 +21,8 @@ def bonus
   }
 
   #code your solution here:
-:hero[:status]= "dead"
+:hero = hero[]
+hero[:status] = "dead"
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
